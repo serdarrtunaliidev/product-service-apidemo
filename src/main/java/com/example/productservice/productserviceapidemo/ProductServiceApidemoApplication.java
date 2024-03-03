f85117b45f17cb489305ecc2f6d22e7949986618
serdarrtunaliidev <hawmns@gmail.com>
2024-03-03 18:05:23 +0300

    first commit

---
+++ src/main/java/com/example/productservice/productserviceapidemo/ProductServiceApidemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.productservice.productserviceapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApidemoApplication.class, args);
+	}
+
+}
